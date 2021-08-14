aad00b3395edcbbdf5df1f3e3e10d6a7f02e5689
Zupiyahh <zupiyah.ha@mail.wu.ac.th>
2021-08-15 02:10:39 +0700

    edit

--- projectconnectdatabasenoswicth/arduino/arduino.ino
+++ projectconnectdatabasenoswicth/arduino/arduino.ino
@@ -10,36 +10,384 @@ void actionMode(char Mode) {
 
   switch (Mode) {
     case '1':
-      Time = 10;
+      Time = 90;
+      for (i = Time; i > 0; i--) {
+        delay(6000);
+        NodeSerial.write(i);
+
+        Serial.print("\nTime is :");
+        Serial.println(i);
+        Serial.print("process is :");
+
+        if (i > 80 && i <= 90) {
+          NodeSerial.println("Water");
+          Serial.println("Water");
+        }
+        else if (i > 61 && i <= 80) {
+          NodeSerial.println("Wash");
+          Serial.println("Wash");
+        }
+        else if (i > 53 && i <= 61) {
+          NodeSerial.println("WasteWater");
+          Serial.println("WasteWater");
+        }
+        else if (i > 43 && i <= 53) {
+          NodeSerial.println("Water");
+          Serial.println("Water");
+        }
+        else if (i > 24 && i <= 43) {
+          NodeSerial.println("RinseWash");
+          Serial.println("RinseWash");
+        }
+        else if (i > 16 && i <= 24) {
+          NodeSerial.println("WasteWater");
+          Serial.println("WasteWater");
+        }
+        else if (i >= 2 && i <= 16) {
+          NodeSerial.println("SpinDrying");
+          Serial.println("SpinDrying");
+        }
+        else if (i == 1) {
+          NodeSerial.println("finishedWashing!!");
+          Serial.println("finishedWashing!!");
+        }
+
+        NodeSerial.flush();
+      }
       break;
+
     case '2':
-      Time = 20;
+      Time = 101;
+      for (i = Time; i > 0; i--) {
+        delay(6000);
+        NodeSerial.write(i);
+
+        Serial.print("\nTime is :");
+        Serial.println(i);
+        Serial.print("process is :");
+
+        if (i > 89 && i <= 101) {
+          NodeSerial.println("Water");
+          Serial.println("Water");
+        }
+        else if (i > 69 && i <= 89) {
+          NodeSerial.println("Wash");
+          Serial.println("Wash");
+        }
+        else if (i > 59 && i <= 69) {
+          NodeSerial.println("WasteWater");
+          Serial.println("WasteWater");
+        }
+        else if (i > 47 && i <= 59) {
+          NodeSerial.println("Water");
+          Serial.println("Water");
+        }
+        else if (i > 27 && i <= 47) {
+          NodeSerial.println("RinseWash");
+          Serial.println("RinseWash");
+        }
+        else if (i > 17 && i <= 27) {
+          NodeSerial.println("WasteWater");
+          Serial.println("WasteWater");
+        }
+        else if (i >= 2 && i <= 17) {
+          NodeSerial.println("SpinDrying");
+          Serial.println("SpinDrying");
+        }
+        else if (i == 1) {
+          NodeSerial.println("finishedWashing!!");
+          Serial.println("finishedWashing!!");
+        }
+
+        NodeSerial.flush();
+      }
       break;
-    default:
+
+    case '3':
+      Time = 61;
+      for (i = Time; i > 0; i--) {
+        delay(6000);
+        NodeSerial.write(i);
+
+        Serial.print("\nTime is :");
+        Serial.println(i);
+        Serial.print("process is :");
+
+
+        if (i> 54 && i <= 61) {
+          NodeSerial.println("Water");
+          Serial.println("Water");
+        }
+        else if (i > 41 && i <= 54) {
+          NodeSerial.println("Wash");
+          Serial.println("Wash");
+        }
+        else if (i > 36 && i <= 41) {
+          NodeSerial.println("WasteWater");
+          Serial.println("WasteWater");
+        }
+        else if (i > 29 && i <= 36) {
+          NodeSerial.println("Water");
+          Serial.println("Water");
+        }
+        else if (i > 16 <= 29) {
+          NodeSerial.println("RinseWash");
+          Serial.println("RinseWash");
+        }
+        else if (i > 11 && i <= 16) {
+          NodeSerial.println("WasteWater");
+          Serial.println("WasteWater");
+        }
+        else if (i >= 2 && i <= 11) {
+          NodeSerial.println("SpinDrying");
+          Serial.println("SpinDrying");
+        }
+        else if (i == 1) {
+          NodeSerial.println("finishedWashing!!");
+          Serial.println("finishedWashing!!");
+        }
+
+        NodeSerial.flush();
+      }
+      break;
+
+    case '4':
+      Time = 47;
+      for (i = Time; i > 0; i--) {
+        delay(6000);
+        NodeSerial.write(i);
+
+        Serial.print("\nTime is :");
+        Serial.println(i);
+        Serial.print("process is :");
+
+
+        if (i > 42 && i <= 47) {
+          NodeSerial.println("Water");
+          Serial.println("Water");
+        }
+        else if (i > 30 && i <= 42) {
+          NodeSerial.println("Wash");
+          Serial.println("Wash");
+        }
+        else if (i > 27 && i <= 30) {
+          NodeSerial.println("WasteWater");
+          Serial.println("WasteWater");
+        }
+        else if (i > 22 && i <= 27) {
+          NodeSerial.println("Water");
+          Serial.println("Water");
+        }
+        else if (i > 10 && i <= 22) {
+          NodeSerial.println("RinseWash");
+          Serial.println("RinseWash");
+        }
+        else if (i > 7 && i <= 10) {
+          NodeSerial.println("WasteWater");
+          Serial.println("WasteWater");
+        }
+        else if (i >= 2 && i <= 7) {
+          NodeSerial.println("SpinDrying");
+          Serial.println("SpinDrying");
+        }
+        else if (i == 1) {
+          NodeSerial.println("finishedWashing!!");
+          Serial.println("finishedWashing!!");
+        }
+        NodeSerial.flush();
+      }
+      break;
+
+    case '5':
+      Time = 35;
+      for (i = Time; i > 0; i--) {
+        delay(6000);
+        NodeSerial.write(i);
+
+        Serial.print("\nTime is :");
+        Serial.println(i);
+        Serial.print("process is :");
+
+        if (i > 31 && i <= 35) {
+          NodeSerial.println("Water");
+          Serial.println("Water");
+        }
+        else if (i > 23 && i <= 31) {
+          NodeSerial.println("Wash");
+          Serial.println("Wash");
+        }
+        else if (i > 21 && i <= 23) {
+          NodeSerial.println("WasteWater");
+          Serial.println("WasteWater");
+        }
+        else if (i > 17 && i <= 21) {
+          NodeSerial.println("Water");
+          Serial.println("Water");
+        }
+        else if (i > 9 && i <= 17) {
+          NodeSerial.println("RinseWash");
+          Serial.println("RinseWash");
+        }
+        else if (i > 7 && i <= 9) {
+          NodeSerial.println("WasteWater");
+          Serial.println("WasteWater");
+        }
+        else if (i >= 2 && i <= 7) {
+          NodeSerial.println("SpinDrying");
+          Serial.println("SpinDrying");
+        }
+        else if (i == 1) {
+          NodeSerial.println("finishedWashing!!");
+          Serial.println("finishedWashing!!");
+        }
+
+        NodeSerial.flush();
+      }
+      break;
+
+    case '6':
+      Time = 60;
+      for (i = Time; i > 0; i--) {
+        delay(6000);
+        NodeSerial.write(i);
+
+        Serial.print("\nTime is :");
+        Serial.println(i);
+        Serial.print("process is :");
+
+
+        if (i > 53 && i <= 60) {
+          NodeSerial.println("Water");
+          Serial.println("Water");
+        }
+        else if (i > 40 && i <= 53) {
+          NodeSerial.println("Wash");
+          Serial.println("Wash");
+        }
+        else if (i > 35 && i <= 40) {
+          NodeSerial.println("WasteWater");
+          Serial.println("WasteWater");
+        }
+        else if (i > 28 && i <= 35) {
+          NodeSerial.println("Water");
+          Serial.println("Water");
+        }
+        else if (i > 15 && i <= 28) {
+          NodeSerial.println("RinseWash");
+          Serial.println("RinseWash");
+        }
+        else if (i > 10 && i <= 15) {
+          NodeSerial.println("WasteWater");
+          Serial.println("WasteWater");
+        }
+        else if (i >=2 && i <= 10) {
+          NodeSerial.println("SpinDrying");
+          Serial.println("SpinDrying");
+        }
+        else if (i == 1) {
+          NodeSerial.println("finishedWashing!!");
+          Serial.println("finishedWashing!!");
+        }
+
+        NodeSerial.flush();
+      }
+      break;
+
+    case '7':
       Time = 30;
+      for (i = Time; i > 0; i--) {
+        delay(6000);
+        NodeSerial.write(i);
+
+        Serial.print("\nTime is :");
+        Serial.println(i);
+ 
+
+        if (i > 27 && i <= 30) {
+          NodeSerial.println("Water");
+          Serial.println("process is :Water");
+        }
+        else if (i > 20 && i <= 27) {
+          NodeSerial.println("Wash");
+          Serial.println("process is :Wash");
+        }
+        else if (i > 18 && i <= 20) {
+          NodeSerial.println("WasteWater");
+          Serial.println("process is :WasteWater");
+        }
+        else if (i > 15 && i <= 18) {
+          NodeSerial.println("Water");
+          Serial.println("process is :Water");
+        }
+        else if (i > 8 && i <= 15) {
+          NodeSerial.println("RinseWash");
+          Serial.println("process is :RinseWash");
+        }
+        else if (i > 6 && i <= 8) {
+          NodeSerial.println("WasteWater");
+          Serial.println("process is :WasteWater");
+        }
+        else if (i >= 2 && i <= 6) {
+          NodeSerial.println("SpinDrying");
+          Serial.println("process is :SpinDrying");
+        }
+        else if (i == 1) {
+          NodeSerial.println("finishedWashing!!");
+          Serial.println("process is :finishedWashing!!");
+        }
+
+        NodeSerial.flush();
+      }
       break;
+
+    default:
+      Time = 54;
+      for (i = Time; i > 0; i--) {
+        delay(6000);
+        NodeSerial.write(i);
+
+        Serial.print("\nTime is :");
+        Serial.println(i);
+        Serial.print("process is :");
+
+        if (i > 48 && i <= 54) {
+          NodeSerial.println("Water");
+          Serial.println("Water");
+        }
+        else if (i > 35 && i <= 48) {
+          NodeSerial.println("Wash");
+          Serial.println("Wash");
+        }
+        else if (i > 31 && i <= 35) {
+          NodeSerial.println("WasteWater");
+          Serial.println("WasteWater");
+        }
+        else if (i > 25 && i <= 31) {
+          NodeSerial.println("Water");
+          Serial.println("Water");
+        }
+        else if (i > 12 && i <= 25) {
+          NodeSerial.println("RinseWash");
+          Serial.println("RinseWash");
+        }
+        else if (i > 8 && i <= 12) {
+          NodeSerial.println("WasteWater");
+          Serial.println("WasteWater");
+        }
+        else if (i >= 2 && i <= 8) {
+          NodeSerial.println("SpinDrying");
+          Serial.println("SpinSpinDrying");
+        }
+        else if (i == 1) {
+          NodeSerial.println("finishedWashing!!");
+          Serial.println("finishedWashing!!");
+        }
+
+        NodeSerial.flush();
+      }
+      break;
+
   }
-  for (i = Time; i > 0; i--) {
-    delay(10000);
-    NodeSerial.write(i);
-
-
-    if (i <= 10) {
-      NodeSerial.println("Spin");
-      Serial.println("Spin");
-    }
-    else if (i <= 20) {
-      NodeSerial.println("Rinse");
-      Serial.println("Rinse");
-    }
-    else if (i <= 30) {
-      NodeSerial.println("Wash");
-      Serial.println("Wash");
-    }
-
-    NodeSerial.flush();
-  }
-
 }
 
 void setup() {
--- projectconnectdatabasenoswicth/esp/esp.ino
+++ projectconnectdatabasenoswicth/esp/esp.ino
@@ -1,26 +1,27 @@
-  #include <ESP8266WiFi.h>
-  #include <WiFiClient.h>
-  #include <ESP8266WebServer.h>
-  #include <ESP8266HTTPClient.h>
-  #include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+#include <SoftwareSerial.h>
+#include <WiFiClient.h>
 
-  SoftwareSerial ArduinoSerial(D1, D0); // RX | TX
+WiFiClient wifiClient;
+SoftwareSerial ArduinoSerial(D1, D0); // RX | TX
 
-  //Set these to your desired credentials.
-  const char *ssid = "Zupiyah_2.4GHz";
-  const char *password = "0622067376";
+//Set these to your desired credentials.
+const char *ssid = "Zupiyah_2.4GHz";
+const char *password = "0622067376";
 
-  //Web/Server address to read/write from
-  const char *host = "192.168.1.6";
+//Web/Server address to read/write from
+const char *host = "192.168.1.6";
 
-  int i;
-  int Mode;
-  String s;
+int i;
+int Mode;
+String s;
 
-  void setup() {
+void setup() {
 
   ArduinoSerial.begin(9600);
-  // delay(1000);
+
   Serial.begin(9600);
   WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
   delay(1000);
@@ -42,15 +43,15 @@
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());  //IP address assigned to your ESP
-  }
+}
 
-  void loop() {
+void loop() {
 
-    //ส่วน Hardware
+  //ส่วน Hardware
 
   //ส่งข้อมูล mode ให้ arduino
   while (Serial.available() > 0) {
-     Mode = Serial.read();
+    Mode = Serial.read();
     if ( Mode == '1' || Mode == '2' || Mode == '3' || Mode == '4' || Mode == '5' || Mode == '6' || Mode == '7' || Mode == '8' ) {
       Serial.print(" Mode : ");
       Serial.write(Mode);
@@ -61,22 +62,23 @@
 
   //รับข้อมูล เวลา และ โพรเสจ จาก Arduino
   while (ArduinoSerial.available() > 0) {
-      i = ArduinoSerial.read ();
-      Serial.print("Time is :");
-      Serial.println(i);
-      s = ArduinoSerial.readString();
-      Serial.println(s);
-    
+    i = ArduinoSerial.read ();
+    Serial.print("\nTime is :");
+    Serial.println(i);
+    s = ArduinoSerial.readString();
+    Serial.print("process is :");
+    Serial.print(s);
 
-  //GET Data ตั้ง HTTP Get ตามต้องการ
-   HTTPClient http;
-   String getData, Link;
-   int iData = i;
-  getData = "?WTime=" + String (iData) + "&Process=" + s;
-  Link = "http://192.168.1.6/ProjectConnectDatabaseNoSwicth/add.php" + getData; // Host เหมือนข้างบน
-  Serial.println(Link);
-  http.begin(Link);
-  int httpCode = http.GET();            //Send the request
-  http.end(); //Close connection
+
+    //GET Data ตั้ง HTTP Get ตามต้องการ
+    HTTPClient http;
+    String getData, Link;
+    int iData = i;
+    getData = "?WTime=" + String (iData) + "&Process=" + s;
+    Link = "http://192.168.1.6/ProjectConnectDatabaseNoSwicth/add.php" + getData; // Host เหมือนข้างบน
+    Serial.print(Link);
+    http.begin(wifiClient,Link);
+    int httpCode = http.GET();            //Send the request
+    http.end(); //Close connection
   }
-  }
+}
